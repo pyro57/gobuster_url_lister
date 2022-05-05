5fde247ca320be0351311ae26f6aca006b29546d
pyro57 <kevinpyrosc@gmail.com>
2022-05-05 15:19:31 -0500

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use std::fs;
+use std::env;
+use std::io:: Write;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let usage = "
+USAGE
+gobuster_url_lister /path/to/gobuster/output/file /path/to/save/urls/
+    ";
+    let mut urls: Vec<String>  = Vec::new();
+    if args.len() != 3{
+        print!("{}", usage);
+    }
+    else{
+        let inputfile = &args[1];
+        let mut outfile = fs::File::create(&args[2]).expect("Failed to open output file");
+        let file_content = fs::read_to_string(inputfile).expect("Error creating reading file");
+        let chunks: Vec<&str> = file_content.split("===============================================================\n").collect();
+        let mut base = String::new();
+        let mut url = String::new();
+        let mut path = String::new();
+        for chunk in chunks{
+            if chunk.len() > 0{
+                let lines: Vec<&str> = chunk.split("\n").collect();
+                for line in lines{
+                    if line.contains("[+] Url:                     "){
+                        let base_vec: Vec<&str> = line.split("[+] Url:                     ").collect();
+                        base = base_vec[1].to_string();
+                    }
+                    if line.contains("(Status:"){
+                        let path_vec: Vec<&str> = line.split("(Status:").collect();
+                        path = path_vec[0].trim().to_string();
+
+                    }
+                    if path.len() > 1{
+                        url = format!("{}{}", base, path);
+                        if urls.contains(&url.to_string()) == false{
+                            urls.push(url);
+                        }  
+                    }
+                    path = String::new();
+                }
+            }
+        }
+        
+        for url in urls{
+            println!("URL = {}", url);
+            writeln!(&mut outfile, "{}", url).expect("failed to write line to output file");
+        }
+    }
+}
